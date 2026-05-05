9335e219e41ee4bb0d9b0c424581ef5e03b096ee
Enzo Carvalho <enzocarvalho33@gmail.com>
2026-05-05 10:38:20 -0300

    feat: script de extração BCB completo

--- src/extract/bcb.py
+++ src/extract/bcb.py
@@ -15,4 +15,22 @@ def fetch_bcb_series(codigo, nome, data_inicio, data_fim):
     df['data'] = pd.to_datetime(df['data'], format='%d/%m/%Y')
     df[nome] = pd.to_numeric(df[nome])
     return df
-    if __name__ == '__main__':
+if __name__ == '__main__':
+    SELIC = 11
+    IPCA = 433
+    CAMBIO = 1
+    
+    data_inicio = '01/01/2020'
+    data_fim = datetime.today().strftime('%d/%m/%Y')
+
+    print('Buscando Selic...')
+    selic = fetch_bcb_series(SELIC, 'selic', data_inicio, data_fim)
+    print(selic.head())
+
+    print('Buscando IPCA...')
+    ipca = fetch_bcb_series(IPCA, 'ipca', data_inicio, data_fim)
+    print(ipca.head())
+
+    print('Buscando Câmbio...')
+    cambio = fetch_bcb_series(CAMBIO, 'cambio_usd', data_inicio, data_fim)
+    print(cambio.head())
